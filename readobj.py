41d04362450e286382d2fdf78ee0f606022bd9e9
Bhupendra Aole <baole@imminet.com>
2017-11-16 13:21:04 -0500

    load obj file. mouse controls.

--- readobj.py
+++ readobj.py
@@ -8,6 +8,7 @@ __version__ = '0.1.0'
 class Obj3D:
     def __init__( self, filename ):
         self.vertices = []
+        self.normals = []
         self.faces = []
 
         f = open( filename )
@@ -20,25 +21,39 @@ class Obj3D:
                     vec=[float(x) for x in line[2:].split()]
                     self.vertices.append( vec )
                 
+                # normals
+                if line[:2]=='vn ':
+                    vec=[float(x) for x in line[3:].split()]
+                    self.normals.append( vec )
+                
                 # faces
                 elif line[:2]=='f ':
                     cs = line[2:].split()
                     face = []
                     for i in cs:
                         c = i.split('/')
-                        face.append( int( c[0] )-1 )
+                        face.append( ( int( c[0] )-1, int( c[2] )-1 ) )
                     self.faces.append( face )
                     
             # read next line
             line=f.readline()
 
-obj = Obj3D( 'testdata\cube.obj' )
+    def getVerticesFlat( self ):
+        v = []
+        for f in self.faces:
+            for i in range( 3 ):
+                v.append( self.vertices[f[i][0]] )
+        return v
+        
+if __name__ == '__main__':
+    obj = Obj3D( 'testdata\cube.obj' )
 
-print( 'Vertices:' )
-for v in range( len( obj.vertices ) ):
-    print( v, obj.vertices[v] )
+    print( 'Vertices:' )
+    for v in range( len( obj.vertices ) ):
+        print( v, obj.vertices[v] )
 
-print( '\nFaces:' )
-for f in range( len( obj.faces ) ):
-    print( str( f+1 ), obj.faces[f] )
-    
+    print( '\nFaces:' )
+    for f in range( len( obj.faces ) ):
+        print( str( f+1 ), obj.faces[f] )
+        
+    print( obj.getVerticesFlat() )
--- wxpythonopengl.py
+++ wxpythonopengl.py
@@ -1,12 +1,39 @@
 
 import wx
+import numpy as np
+import time
+
 from wx import glcanvas
 
 from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.arrays import vbo
+from OpenGL.GL import shaders
+
+from readobj import Obj3D
+
+vertexShader = """
+    #version 120
+    void main() {
+        gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+    }
+    """
+
+fragmentShader = """
+    #version 120
+    void main() {
+        gl_FragColor = vec4( .9, .9, .9, 1 );
+    }
+    """
 
 class GLFrame( glcanvas.GLCanvas ):
     """A simple class for using OpenGL with wxPython."""
-
+    
+    near_plane = 0.1
+    far_plane = 100
+    world_pos = (0, 0, -6)
+    world_rot = (0, 0, 0)
+    
     def __init__(self, parent):
         self.GLinitialized = False
         attribList = (glcanvas.WX_GL_RGBA, # RGBA
@@ -19,11 +46,17 @@ class GLFrame( glcanvas.GLCanvas ):
         # Create the canvas
         self.context = glcanvas.GLContext( self )
 
+        self.left_down = False
+        
         #
         # Set the event handlers.
         self.Bind(wx.EVT_ERASE_BACKGROUND, self.processEraseBackgroundEvent)
         self.Bind(wx.EVT_SIZE, self.processSizeEvent)
         self.Bind(wx.EVT_PAINT, self.processPaintEvent)
+        self.Bind(wx.EVT_MOUSEWHEEL, self.processWheelEvent)
+        self.Bind(wx.EVT_MOTION, self.processMotion)
+        self.Bind(wx.EVT_LEFT_DOWN, self.processLeftDown)
+        self.Bind(wx.EVT_LEFT_UP, self.processLeftUp)
 
     #
     # Canvas Proxy Methods
@@ -35,24 +68,41 @@ class GLFrame( glcanvas.GLCanvas ):
     #
     # wxPython Window Handlers
 
-    def processEraseBackgroundEvent(self, event):
+    def processLeftDown( self, event ):
+        self.last_pos = event.GetPosition()
+        self.left_down = True
+        
+    def processLeftUp( self, event ):
+        self.left_down = False
+        
+    def processMotion( self, event ):
+        if self.left_down:
+            pos = event.GetPosition()
+            diff = (pos-self.last_pos)
+            self.world_rot = ( self.world_rot[0]+diff[1], self.world_rot[1]+diff[0], self.world_rot[2] )
+            # print(  )
+            self.last_pos = pos
+            self.Refresh( False )
+        
+    def processWheelEvent( self, event ):
+        delta = event.GetWheelRotation() / 100
+        self.world_pos = ( self.world_pos[0], self.world_pos[1], self.world_pos[2]+delta )
+        self.Refresh( False )
+        
+    def processEraseBackgroundEvent( self, event ):
         """Process the erase background event."""
         pass # Do nothing, to avoid flashing on MSWin
 
-    def processSizeEvent(self, event):
-        """Process the resize event."""
-        #if self.GetContext():
-            # Make sure the frame is shown before calling SetCurrent.
+    def processSizeEvent( self, event ):
         self.Show()
         self.SetCurrent( self.context )
 
         size = self.GetGLExtents()
-        self.OnReshape(size.width, size.height)
-        self.Refresh(False)
+        self.OnReshape( size.width, size.height )
+        self.Refresh( False )
         event.Skip()
 
     def processPaintEvent(self, event):
-        """Process the drawing event."""
         self.SetCurrent( self.context )
 
         # This is a 'perfect' time to initialize OpenGL ... only if we need to
@@ -69,32 +119,49 @@ class GLFrame( glcanvas.GLCanvas ):
     def OnInitGL(self):
         """Initialize OpenGL for use in the window."""
         glClearColor(1, 1, 1, 1)
+        
+        VERTEX_SHADER = shaders.compileShader( vertexShader, GL_VERTEX_SHADER )
+        FRAGMENT_SHADER = shaders.compileShader( fragmentShader, GL_FRAGMENT_SHADER )
+        
+        self.shader = shaders.compileProgram( VERTEX_SHADER, FRAGMENT_SHADER )
 
-    def OnReshape(self, width, height):
+        cube = Obj3D( 'testdata\cube.obj' )
+        data = cube.getVerticesFlat()
+        self.vbo = vbo.VBO( np.array( data, 'f' ) )
+        
+    def OnReshape( self, width, height ):
         """Reshape the OpenGL viewport based on the dimensions of the window."""
-        glViewport(0, 0, width, height)
-
-        glMatrixMode(GL_PROJECTION)
+        glViewport( 0, 0, width, height )
+        
+        glMatrixMode( GL_PROJECTION )
         glLoadIdentity()
-        glOrtho(-0.5, 0.5, -0.5, 0.5, -1, 1)
+        # glOrtho( -0.5, 0.5, -0.5, 0.5, -1, 1 )
+        gluPerspective( 45.0, width/height, self.near_plane, self.far_plane )
 
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
-    def OnDraw(self, *args, **kwargs):
-        "Draw the window."
-        glClear(GL_COLOR_BUFFER_BIT)
-
-        # Drawing an example triangle in the middle of the screen
-        glBegin(GL_TRIANGLES)
-        glColor(0, 0, 0)
-        glVertex(-.25, -.25)
-        glVertex(.25, -.25)
-        glVertex(0, .25)
-        glEnd()
-
+    def OnDraw( self ):
+        glPushMatrix()
+        glTranslate( self.world_pos[0], self.world_pos[1], self.world_pos[2] )
+        glRotated( self.world_rot[1], 0, 1, 0 )
+        glRotated( self.world_rot[0], 1, 0, 0 )
+        
+        glClear( GL_COLOR_BUFFER_BIT )
+        
+        shaders.glUseProgram( self.shader )
+        self.vbo.bind()
+        glEnableClientState( GL_VERTEX_ARRAY );
+        glVertexPointerf( self.vbo )
+        glDrawArrays( GL_TRIANGLES, 0, len( self.vbo ) )
+        self.vbo.unbind()
+        glDisableClientState( GL_VERTEX_ARRAY );
+        shaders.glUseProgram( 0 )
+        
+        glPopMatrix()
+        
         self.SwapBuffers()
-
+            
 class Window( wx.Frame ):
     def __init__( self, *args, **kwargs ):
         super().__init__( *args, **kwargs )
